aff337c4126c28e823f8cbaf614f367290098130
Shagan742 <sanghashagan@gmail.com>
2026-03-15 13:03:37 -0400

    finished home pg + finished footer + finished navbar

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 const app = Vue.createApp({
+    created() {
+    fetch('reviews.json').then(response => response.json()).then(json => {
+      this.reviews = json
+      console.log(this.reviews)
+    })
+  },
     data() {
         return {
+            reviews: []
+        }
+    },
+    methods: {
+
+    },
+    computed: {
+        threeCardsPerSlide() {
+            const three=[];
+            for(let i=0; i<this.reviews.length; i+=3) {
+               three.push(this.reviews.slice(i, i+3))
+            }
+            return three;
         }
     }
 }).mount('#app')
